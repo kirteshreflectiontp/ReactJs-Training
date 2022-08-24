31520ab95ab419b86feed0daddeb1cf30612570a
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-08-25 00:02:51 +0530

    1st try

---
+++ my-app/src/project/header.js
@@ -0,0 +1,60 @@
+
+const Header = () => {
+    return(
+        <>
+        <header>
+    <section class="header_bg">
+        <div class="header_bg2"></div>
+    </section>
+    <section class="header_content">
+        <div class="head_one">
+            <div><img src="./images/Place Your Logo Here (Double Click to Edit).png " alt="logo" width="251"
+                    height="63"/></div>
+            <div>
+                <div class="head_one_txt">
+                    <div class="head_one_txt1">EXPRESS DELIVERY</div>
+                    <div class="head_one_txt2">1800 &nbsp; 258 &nbsp; 888</div>
+                </div>
+                <div class="head_one_txt3"><img src="images/iPhone.png" alt="phone" width="29" height="53"/></div>
+            </div>
+        </div>
+        <nav class="head_two">
+            <div></div>
+            <div class="head_nav">
+                <span class="head_nav_item">H O M E</span>
+                <span class="head_nav_item">M E N U</span>
+                <span class="head_nav_item">O U R &nbsp; S T O R Y</span>
+                <span class="head_nav_item">C O N T A C T &nbsp; U S</span>
+            </div>
+        </nav>
+        <div class="head_three">
+            <div>
+                <div class="header_txt1">Are you Hungry?</div>
+                <div class="header_txt2">DONT WAIT!</div>
+                <div class="header_txt3">LET START TO ORDER FOOD NOW</div>
+                <div class="head_border"></div>
+                <div class="header_txt4">
+                    <div>
+                        <div>THIS MONDAY</div>
+                        <div>HAPPY HOURS</div>
+                    </div>
+                    <div class="head_num">1+1=3</div>
+                </div>
+                <button class="order_now_button">ORDER NOW</button>
+            </div>
+            <div>
+                <img src="./images/Leaf1.png" alt="" class="head_leaf" width="45" height="105"/>
+                <img src="./images/Pasta.png" alt="" class="head_pasta" width="95" height="96"/>
+                <img src="./images/Place Your Image Here (Double Click to Edit)1.png" alt="dish"
+                    class="head_pasta_dish" width="567" height="567"/>
+                <img src="./images/Leaf.png" alt="" class="head_leaf1" width="83" height="109"/>
+                <img src="./images/Tomato1.png" alt="" class="head_tomato" width="81" height="81"/>
+                <img src="./images/Spaghetti.png" alt="" class="head_spaghetti" width="305" height="298"/>
+            </div>
+        </div>
+    </section>
+</header>
+        </>
+    )
+}
+export default Header;
