3b0235053e87efc9fd6f679b3637ad88f3c03368
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-08-29 23:09:15 +0530

    login form

--- assignments/src/(29082022)/footer/footer.js
+++ assignments/src/(29082022)/footer/footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     return(
 <>
 <div className="foot">
-    <div>All copyright reserved Library of Life</div>
+    <div>All copyright &copy; reserved Library of Life</div>
     <div><span className='termscondition'>Terms & Conditions</span><span>Privacy Policy</span> </div>
 </div>
 
