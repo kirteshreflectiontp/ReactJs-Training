5427a57e2e1b54daa8d1883f264a2c90393658b2
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-08-31 11:04:18 +0530

    change without refresh

--- assignments/src/(30082022)/loginform.js
+++ assignments/src/(30082022)/loginform.js
@@ -11,7 +11,10 @@ const Form = () => {
 <div className='body_b'>
 <div className="head">
     <div className ='logo'>LoGo</div>
+   
     <div><button className='signup_button' onClick={(event)=> pagechange(event)}>{page === 'LOGIN'?'SIGN UP':'LOGIN'}</button></div>
+   
+
 </div>
 <div className="main">
     <div className ='login_head'>Login</div>
@@ -20,8 +23,10 @@ const Form = () => {
 <br/>
 <br/>
 <br/>
+{page === 'LOGIN'?
 <div>
-<div className='input-data'>
+
+            <div className='input-data'>
                 <Inputprops type="text" placeholder="Email Address"/>
             </div>
             <div className='input-data'>
@@ -31,16 +36,31 @@ const Form = () => {
             <br/>
             <br/>
             <br/>
-            {page === 'LOGIN'?
             <div className='input-data'>
                 <button className='login_button'>LOGIN</button>
             </div>
-            :
+</div>
+ :
+ <div>
+            <div className='input-data'>
+                <Inputprops type="text" placeholder="Email Address"/>
+            </div>
+            <div className='input-data'>
+                <Inputprops  type="password" placeholder="Password"/>
+            </div>
+            <div className='input-data'>
+                <Inputprops  type="number" placeholder="Mobile Number"/>
+            </div>
+            <div className='forgot'><span>Forgot Password?</span> </div>
+            <br/>
+            <br/>
+            <br/>
             <div className='input-data'>
                 <button className='login_button'>SIGN UP</button>
-            </div>
-            }
+            </div>        
 </div>
+  }  
+
 <div className="foot">
     <div>All copyright &copy; reserved Library of Life</div>
     <div><span className='termscondition'>Terms & Conditions</span><span>Privacy Policy</span> </div>
