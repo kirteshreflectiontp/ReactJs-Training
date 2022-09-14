02196af6cb517e8c70348c2e581e2a560e74f3a2
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-09-15 00:20:04 +0530

    useeffect

--- assignments/src/(08092022)/header.js
+++ assignments/src/(08092022)/header.js
@@ -1,4 +1,4 @@
-import Inputprops from '../(30082022)/inputprops';
+
 import './header.css';
 import React,{useState} from 'react';
 import Nav from './navlinks/nav';
@@ -6,13 +6,17 @@ import Nav from './navlinks/nav';
 
 const Header = () => {
     const [dp, setDp] = useState(false);
+    const [value,setValue] = useState();
+    React.useEffect(()=>{
+        console.log('InputChanges', value);
+    },[value])
     
     return (
 <>
 <div className="parent_header">
     <div>
         <span className='logo'>LoGo</span>
-        <span><Inputprops className='search_input' type="text" placeholder="Search Users by Name" /><i class="fa-solid fa-magnifying-glass search_icon"></i></span>
+        <span><input onChange={(e) => setValue(e.target.value)} className='search_input' type="text" placeholder="Search Users by Name" /><i class="fa-solid fa-magnifying-glass search_icon"></i></span>
     </div>
    
     <div className='header_clicks'>
