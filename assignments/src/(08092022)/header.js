a0906817452c2bd1f0a9ad3c8c25449c2c0e709d
kirteshreflectiontp <kirtesh.reflectiontp@gmail.com>
2022-10-08 02:43:08 +0530

    project

--- assignments/src/(08092022)/header.js
+++ assignments/src/(08092022)/header.js
@@ -31,6 +31,21 @@ const Header = () => {
     }
     showVideoPopUp(false)
    }
+   const [videoNav,setVideoNav] = useState('youtube');
+   const switchVideoUpload = (nav) => {
+    setVideoNav(nav)
+    setVideoURL('');
+   }
+   const setVideoURLFromLocal=(event) => {
+    if(event.target.files[0]?.type ==='video/mp4'){
+      console.log(event.target.files[0])
+      setVideoURL(event.target.files[0])
+    }
+    else{
+      setVideoURL('');
+    }
+
+   } 
     return (
 <>
 <div className="parent_header">
@@ -52,17 +67,31 @@ const Header = () => {
     <div className='Upload_video-Overlay' onClick={() => showVideoPopUp(false)}></div>
     <>
     <div className='Video_upload'>
+      <div className='choose_upload'>
+        <div onClick={() => switchVideoUpload('youtube')} className={`${videoNav ==='youtube'? 'active':''}`}>Youtube</div>
+        <div onClick={() => switchVideoUpload('computer')} className={`${videoNav ==='computer'? 'active':''}`}>My Computer</div>
+      </div>
     <div className='Video_URL'>
     <h2>Upload Video</h2>
     <input type='text' value= {videoTitle} onChange ={(event) => setVideoTitle(event.target.value)} placeholder='Enter Video Tittle Here'/>
+   
     </div>
     <div className='Video_URL'>
+    {videoNav ==='youtube' &&
     <input type='text'value={videoURL} onChange = {(event) => setVideoURL(event.target.value)} placeholder='Enter Youtube Embeded URL here'/>
+    }
+    {videoNav ==='computer' &&
+   <input type= 'file' onChange={(event) => setVideoURLFromLocal(event)}/>
+    }
     </div>
     <div>
       <button onClick = {uploadVideo} >Upload</button>
     </div>
-    
+    <div>
+    {videoURL &&
+   <video controls><source src={`./${videoURL.name}`}/></video>
+  }
+    </div>
   </div>
   </>
     </div>
